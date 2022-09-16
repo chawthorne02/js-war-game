ea98e3a8f5ae565b0ebbe5e97ff0ca29e0ade34e
Corey Hawthorne <chawthorne02@gmail.com>
2022-09-16 10:41:33 -0400

    added player 1 and 2 inputs

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const SUITS = [""]
+const VALUES = [ "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" ]
+
+
+
+class Deck {
+    constructor(cards) {
+        this.cards = cards;
+    }
+}
