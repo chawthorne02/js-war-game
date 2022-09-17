f08ca8a90e3ca8482c459a7d7d148f33d13305a2
Corey Hawthorne <chawthorne02@gmail.com>
2022-09-17 12:54:30 -0400

    added shuffle deck function

--- main.js
+++ main.js
@@ -4,9 +4,23 @@ const VALUES = [ "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K
 
 
 class Deck {
-    constructor(cards = deckOfCards ) {
+    constructor(cards = deckOfCards()) {
         this.cards = cards;
     }
+
+get numberOfCards(){
+    return this.cards.length;
+}
+
+// function to shuffle the deck
+shuffleDeck(){
+    for (let i = this.numberOfCards - 1; i > 0; i--) {
+        const newIndex = Math.floor(Math.random() * ( i + 1 ));
+        const oldValue = this.cards[newIndex];
+        this.cards[newIndex] = this.cards[i];
+        this.cards[i] = oldValue
+    }
+}
 }
 
 class Card {
@@ -15,9 +29,9 @@ class Card {
         this.value = value;
     }
 }
-
+//  creates deck of cards
 function deckOfCards() {
-    return SUITS.flatMap(suit => {
+    return SUITS.flatMap(suit => {   // flatMap turns an array of arrays into a single array
         return VALUES.map(value => {
             return new Card(suit, value)
         })
@@ -25,3 +39,6 @@ function deckOfCards() {
 }
 
 const deck = new Deck()
+deck.shuffleDeck();
+console.log(deck.cards)
+
